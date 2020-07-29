58850016afd74d42da6f30fa334d3fb713815066
Garves <garves44@gmail.com>
2020-07-29 12:43:36 -0600

    updated Category-routes updated all models

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,23 @@
-const { Model, DataTypes } = require('sequelize');
+const {
+  Model,
+  DataTypes
+} = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
-Category.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
-);
+Category.init({
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      category_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    );
 
-module.exports = Category;
+    module.exports = Category;
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,109 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const {
+  Category,
+  Product
+} = require('../../models');
+const { update } = require('../../models/Tag');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+      attributes: [
+        'id',
+        'category_name',
+        'created_at',
+        [
+
+        ]
+      ],
+      order: [
+        ['created_at', 'DESC']
+      ],
+      include: [{
+        model: Product,
+        attributes: ['product_name']
+      }]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'category_name',
+        'created_at',
+        [
+
+        ]
+      ],
+      include: [{
+        model: Product,
+        attributes: ['product_name']
+      }]
+    })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({
+          message: 'No category with this id!'
+        });
+        return;
+      }
+      res.json(dbPostData)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+    category_name: req.param.category_name
+  })
+  .then(dbPostData => res.json(dbPostData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(req.body, { Category })
+  .then(updatedPostData => res.json(updatedPostData))
+  .catch(err => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbPostData => {
+    if(!dbPostData) {
+      res.status(404).json({
+        message: 'No Category with this ID!'
+      });
+      return;
+    }
+    res.json(dbPostData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
